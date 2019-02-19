18fd45cde520d7473f131ea3090d1a6fb2438914
EC2 Default User <ec2-user@ip-172-31-87-130.ec2.internal>
2019-02-19 11:21:20 +0000

    fitst commit-add all

---
+++ carousel.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true,
+            startPosition:0,
+        }
+    );
+    $("#menu").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+});
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
